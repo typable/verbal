a0dc09f7eede290f593f7e739acbfea057294cd7
typable <typable@prototype-studio.de>
2022-04-12 20:16:20 +0200

    Removed comments

--- src/test.rs
+++ src/test.rs
@@ -1,39 +1,3 @@
-// #[macro_use]
-// extern crate diesel;
-
-// use diesel::pg::PgConnection;
-// use diesel::prelude::*;
-
-// mod api;
-// mod data;
-// mod error;
-// mod model;
-// mod rest;
-// mod schema;
-// mod utils;
-
-// use api::{RadioBrowserApi, Search};
-// use model::*;
-// use schema::account::dsl::*;
-
-// #[async_std::main]
-// async fn main() {
-//     let database_url = "postgres://typable:De24Si98@192.168.2.3:9748/verbal";
-//     let conn = PgConnection::establish(&database_url).expect("Unable to connect to database!");
-//     let results = account
-//         .load::<Account>(&conn)
-//         .expect("Unable to load model!");
-//     for item in results {
-//         println!("{:?}", item);
-//     }
-//     let mut search = Search::default();
-//     search.name = "bbc".into();
-//     match RadioBrowserApi::search(search).await {
-//         Ok(result) => println!("{:#?}", result),
-//         Err(err) => println!("{}", err),
-//     }
-// }
-
 mod model;
 
 use sqlx::postgres::PgPool;
