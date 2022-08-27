78eb7ac28630149703b850bfa27af473be98e8f7
typable <contact@typable.dev>
2022-08-27 13:26:00 +0200

    Improved popup animation

--- app/component/popup.js
+++ app/component/popup.js
@@ -3,31 +3,54 @@ import VButton from '../element/button.js';
 
 export default {
     props: ['title', 'description', 'actions'],
+    data() {
+        return {
+            show: false
+        };
+    },
     components: {
         VButton
     },
     methods: {
         $lang,
         open(info) {
+            console.log(info);
             const {title, description, actions} = info;
             this.title = title;
             this.description = description;
             this.actions = actions;
-            this.$refs.dialog.show();
+            this.show = true;
         },
         close() {
-            this.title = null;
-            this.description = null;
-            this.actions = null;
-            this.$refs.dialog.close();
+            this.show = false;
         }
     },
-    mounted() {
-        this.close();
+    computed: {
+        opacity() {
+            return {
+                'opacity': `${this.show ? 1 : 0}`,
+                'pointer-events': `${this.show ? 'all' : 'none'}`
+            };
+        },
+        transform() {
+            return {
+                'opacity': `${this.show ? 1 : 0}`,
+                'pointer-events': `${this.show ? 'all' : 'none'}`,
+                'transform': `translate(-50%, calc(-50% + ${this.show ? '0' : '30'}px))`
+            };
+        }
     },
     template: `
-        <dialog ref="dialog" class="z-40 fixed w-full h-full bg-black/80">
-            <div class="flex flex-col fixed left-[50%] top-[50%] -translate-y-[50%] -translate-x-[50%] rounded-lg sm:rounded-md bg-zinc-900 w-[500px] max-w-[85vw] p-6 sm:p-8 pt-10 sm:pt-8">
+        <div class="z-40">
+            <div
+                class="fixed top-0 bottom-0 left-0 right-0 bg-black/60 transition-opacity duration-300 ease-in-out"
+                :style="opacity"
+            >
+            </div>
+            <div
+                class="z-50 flex flex-col fixed left-[50%] top-[50%] -translate-y-[50%] -translate-x-[50%] rounded-lg sm:rounded-md bg-zinc-900 w-[500px] max-w-[85vw] p-6 sm:p-8 pt-10 sm:pt-8 transition-all duration-500 ease-in-out"
+                :style="transform"
+            >
                 <h3 class="text-white text-[24px] font-bold pb-4 text-center sm:text-left">{{title}}</h3>
                 <p class="text-md text-gray-400 text-center sm:text-left" v-html="description"></p>
                 <div class="flex gap-2 w-full justify-end">
@@ -39,6 +62,6 @@ export default {
                     ></v-button>
                 </div>
             </div>
-        </dialog>
+        </div>
     `
 }
