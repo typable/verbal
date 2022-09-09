5ae2a87242c1313fa07dc5d347ee1fa52a6809cb
typable <contact@typable.dev>
2022-09-09 20:10:34 +0200

    Fixed padding of detail/not-found page

--- app/component/detail.js
+++ app/component/detail.js
@@ -128,7 +128,7 @@ export default {
                 </div>
                 <div
                     ref="modal"
-                    class="w-full mx-auto flex flex-col bg-black fixed left-[100vw] top-0 h-[100vh] overflow-y-auto px-4 pb-12 sm:pb-[100px] sm:px-10 transition-detail"
+                    class="w-full mx-auto flex flex-col bg-black fixed left-[100vw] top-0 h-[100vh] overflow-y-auto pb-12 sm:pb-[100px] transition-detail"
                     :class="{ 'detail-active': open }"
                 >
                     <div class="w-full flex flex-col px-4 sm:px-10 max-w-[1200px] mx-auto">
--- app/page/notfound.js
+++ app/page/notfound.js
@@ -20,7 +20,7 @@ export default {
             <div class="z-40">
                 <div
                     ref="modal"
-                    class="w-full mx-auto flex flex-col bg-black fixed left-[100vw] top-0 h-[100vh] overflow-y-auto px-4 pb-12 sm:pb-[100px] sm:px-10 transition-detail detail-active"
+                    class="w-full mx-auto flex flex-col bg-black fixed left-[100vw] top-0 h-[100vh] overflow-y-auto pb-12 sm:pb-[100px] transition-detail detail-active"
                 >
                     <div class="w-full flex flex-col px-4 sm:px-10 max-w-[1200px] mx-auto">
                         <div class="w-full h-[98px] py-6 flex gap-4 justify-between">
