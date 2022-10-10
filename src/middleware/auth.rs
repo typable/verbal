1d54f8a7d9f5af35b0cb55bc4e56eef84c5b42a1
typable <contact@typable.dev>
2022-10-10 10:08:44 +0200

    Excluded preflight requests from auth process

--- src/middleware/auth.rs
+++ src/middleware/auth.rs
@@ -18,49 +18,51 @@ impl<State: Clone + Send + Sync + 'static> tide::Middleware<State> for Auth {
         mut req: tide::Request<State>,
         next: tide::Next<'_, State>,
     ) -> tide::Result {
-        let path_url = req.url().path();
-        if path_url.starts_with("/api") {
-            match req.header("verbal-token") {
-                Some(header) => {
-                    let token = header.as_str();
-                    let option_account;
-                    {
-                        let query = format!(
-                            "
-                                SELECT
-                                    account.*,
-                                    (
-                                        SELECT
-                                            sum(playtime)
-                                        FROM get_playtime(account.id)
-                                    )
-                                    AS playtime
-                                    FROM account
-                                    INNER JOIN device
-                                        ON account.id = device.account_id
-                                    WHERE device.uid = '{}'
-                            ",
-                            &token,
-                        );
-                        let mut conn = req.sqlx_conn::<Postgres>().await;
-                        option_account = unwrap_result_or_throw!(
-                            sqlx::query_as::<_, model::Account>(&query)
-                                .fetch_optional(unwrap_result_or_throw!(
-                                    conn.acquire().await,
-                                    "cannot acquire connection to database!"
-                                ))
-                                .await,
-                            "cannot acquire account for given device!"
-                        );
+        if req.method() != Method::Options {
+            let path_url = req.url().path();
+            if path_url.starts_with("/api") {
+                match req.header("verbal-token") {
+                    Some(header) => {
+                        let token = header.as_str();
+                        let option_account;
+                        {
+                            let query = format!(
+                                "
+                                    SELECT
+                                        account.*,
+                                        (
+                                            SELECT
+                                                sum(playtime)
+                                            FROM get_playtime(account.id)
+                                        )
+                                        AS playtime
+                                        FROM account
+                                        INNER JOIN device
+                                            ON account.id = device.account_id
+                                        WHERE device.uid = '{}'
+                                ",
+                                &token,
+                            );
+                            let mut conn = req.sqlx_conn::<Postgres>().await;
+                            option_account = unwrap_result_or_throw!(
+                                sqlx::query_as::<_, model::Account>(&query)
+                                    .fetch_optional(unwrap_result_or_throw!(
+                                        conn.acquire().await,
+                                        "cannot acquire connection to database!"
+                                    ))
+                                    .await,
+                                "cannot acquire account for given device!"
+                            );
+                        }
+                        req.set_ext(unwrap_option_or_throw!(
+                            option_account,
+                            "no account found for given token!"
+                        ));
                     }
-                    req.set_ext(unwrap_option_or_throw!(
-                        option_account,
-                        "no account found for given token!"
-                    ));
-                }
-                None => {
-                    if !(path_url.eq("/api/account") && req.method() == Method::Post) {
-                        return Response::throw("no token was provided!");
+                    None => {
+                        if !(path_url.eq("/api/account") && req.method() == Method::Post) {
+                            return Response::throw("no token was provided!");
+                        }
                     }
                 }
             }
